723fb67cd4e2866063820195d43f27fdfb64f85a
LSantanderGit <lucasmsantander136@gmail.com>
2023-12-13 15:34:38 -0300

    feat: Init and BootStrap

---
+++ grails/grails-qst-movies/grails-app/domain/qst/movies/Movies.groovy
@@ -0,0 +1,25 @@
+package qst.movies
+
+class Movies {
+
+    String title
+    String description 
+    String rating
+    String duration
+    String releasedDate
+    String trailerLink
+    String posterDir
+    List<String> genre
+
+    static constraints = {
+        title nullable: false
+        description nullable: false
+        rating nullable: false
+        duration nullable: false
+        releasedDate nullable: false
+        trailerLink nullable: false
+        posterDir nullable: false
+        genre nullable: false
+    }
+
+}
--- grails/grails-qst-movies/grails-app/init/grails/qst/movies/BootStrap.groovy
+++ grails/grails-qst-movies/grails-app/init/grails/qst/movies/BootStrap.groovy
@@ -1,8 +1,10 @@
 package grails.qst.movies
+import qst.init.Init
 
 class BootStrap {
 
     def init = { servletContext ->
+        Init.begin()
     }
     def destroy = {
     }
