d602322c159f2d5b8308a6a71a0a0e39661ab54e
LSantanderGit <lucasmsantander136@gmail.com>
2023-12-21 17:03:23 -0300

    feat: added bootstrap

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,14 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppListMoviesComponent } from './components/app-list-movies/app-list-movies.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule, AppListMoviesComponent],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, AppListMoviesComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Santander-QST-peliculas';
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
